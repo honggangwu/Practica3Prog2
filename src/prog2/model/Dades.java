d68ed348775f36c20fa7a841fbee19d9b178fb39
honggangwu <honggangwu25012005@gmail.com>
2026-05-07 13:23:59 +0200

    primea

---
+++ src/prog2/model/Dades.java
@@ -0,0 +1,47 @@
+package prog2.model;
+
+import prog2.vista.BiblioException;
+
+import java.util.ArrayList;
+
+public class Dades implements InDades {
+    @Override
+    public void afegirExemplar(String id, String titol, String autor, boolean admetPrestecLlarg) throws BiblioException {
+
+    }
+
+    @Override
+    public ArrayList<Exemplar> recuperaExemplars() {
+        return null;
+    }
+
+    @Override
+    public void afegirUsuari(String email, String nom, String adreca, boolean esEstudiant) throws BiblioException {
+
+    }
+
+    @Override
+    public ArrayList<Usuari> recuperaUsuaris() {
+        return null;
+    }
+
+    @Override
+    public void afegirPrestec(int exemplarPos, int usuariPos, boolean esLlarg) throws BiblioException {
+
+    }
+
+    @Override
+    public void retornarPrestec(int position) throws BiblioException {
+
+    }
+
+    @Override
+    public ArrayList<Prestec> recuperaPrestecs() {
+        return null;
+    }
+
+    @Override
+    public ArrayList<Prestec> recuperaPrestecsNoRetornats() {
+        return null;
+    }
+}
