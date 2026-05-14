8e1962fef21263003d3c0679759fc0060bde635e
honggangwu <honggangwu25012005@gmail.com>
2026-05-14 13:53:00 +0200

    part2

--- src/prog2/vista/AppBiblioUB.java
+++ src/prog2/vista/AppBiblioUB.java
@@ -3,4 +3,24 @@ package prog2.vista;
 import javax.swing.*;
 
 public class AppBiblioUB extends JFrame {
+    private JPanel panelMain;
+    private JButton btnGestioUsuaris;
+    private JButton btnGestioExemplars;
+    private JButton tnGestioPrestecs;
+    private JButton btnGuardar;
+    private JButton btnCarregar;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            AppBiblioUB biblioUB = new AppBiblioUB();
+            biblioUB.setVisible(true);
+        });
+    }
+    public AppBiblioUB(){
+        setTitle("AppBiblioUB GUI");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setContentPane(panelMain);
+        setSize(500,400);
+        setLocationRelativeTo(null);
+    }
 }
